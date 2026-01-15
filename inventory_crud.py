a43ad2a81bee40cdf9722be1ddb5b7a1387ee8c6
smmigrino <smmigrino@student.apc.edu.ph>
2026-01-15 20:36:25 +0800

    update sku

--- inventory_crud.py
+++ inventory_crud.py
@@ -11,7 +11,7 @@ items = Table("items", metadata, autoload_with=engine)
 with engine.connect() as conn:
     # INSERT (Sample item)
     stmt = insert(items).values(
-        sku="SKU-005",
+        sku="SKU-003",
         name="Sample Item 1",
         quantity=10,
         price=99.99
