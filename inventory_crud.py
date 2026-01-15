28e0ab7a88769687a2d43b6ac94460191338a0d6
smmigrino <smmigrino@student.apc.edu.ph>
2026-01-15 20:17:48 +0800

    create crud file to practice test

---
+++ inventory_crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, MetaData, Table, insert, select
+
+engine = create_engine(
+    "mysql+mysqlconnector://root:@127.0.0.1:3306/sandbox_db",
+    future=True
+)
+
+metadata = MetaData()
+items = Table("items", metadata, autoload_with=engine)
+
+with engine.connect() as conn:
+    # INSERT (Sample item)
+    stmt = insert(items).values(
+        sku="SKU-001",
+        name="Sample Item 1",
+        Quantity=10,
+        price=99.99
+    )
+    
+    conn.execute(stmt)
+    conn.commit()
+    
+    # SELECT (read all items)
+    result = conn.execute(select(items))
+    for row in result:
+        print(dict(row))
