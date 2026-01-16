52c08b0744c2496156b6408af2e77e76adf34f53
smmigrino <smmigrino@student.apc.edu.ph>
2026-01-16 22:23:06 +0800

    created text read file to test print

---
+++ test_read.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, MetaData, Table, update, delete, select
+
+engine = create_engine(
+    "mysql+mysqlconnector://root:@127.0.0.1:3306/sandbox_db",
+    future=True
+)
+
+metadata = MetaData()
+items = Table("items", metadata, autoload_with=engine)
+
+result = conn.execute(select(items))
+for row in result:
+    print(dict(row._mapping))
